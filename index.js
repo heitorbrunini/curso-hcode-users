b6e6b2ce01e9f9825ef3c1d661b733b3eead62bd
heitorbrunini <heitor.brunini@gmail.com>
2022-03-02 02:22:10 -0300

    Form Data

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
+function addLine(dataUser) {
 
-var fields =  document.querySelectorAll("#form-user-create [name]");
-var user = { };
+    document.getElementById("table-users").innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>`
+    ;
+}
 
-fields.forEach(
-    function(field, index){
-        if (field.name =="gender" && field.checked) {
-            user[field.name] = field.ariaValueMax;
-        } else{
-            user[field.name] = field.ariaValueMax;
-        }
+document.getElementById("form-user-create").addEventListener("submit",
+    function (event) {
+        event.preventDefault();
+
+        //captura dos dados ao evento submit dentro do formulário
+        fields.forEach(
+            function (field, index) {
+                if (field.name == "gender" && field.checked) {
+                    user[field.name] = field.value;
+                } else {
+                    user[field.name] = field.value;
+                }
+            }
+        );
+
+        addLine(user);
     }
 );
+
