b6471c74dbe8abcbe2ab5b332348d046ed008c58
heitorbrunini <heitorbrunini.ifpb@gmail.com>
2022-03-03 17:05:55 -0300

    Data and Getters

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -103,7 +103,7 @@ class UserController {
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
-            <td>${dataUser.birth}</td>
+            <td>${dataUser.register}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
