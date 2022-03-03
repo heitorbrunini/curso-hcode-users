cc7bbfec07efcdaba2a013aa847bb8488e573de5
heitorbrunini <heitorbrunini.ifpb@gmail.com>
2022-03-03 02:27:48 -0300

    Load Photo with Promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,16 +11,61 @@ class UserController {
         this.formEl.addEventListener("submit",
             event => {
                 event.preventDefault();
-                this.addLine(this.getValues());
-            }
-        );
+
+                //peega os valores do formúlário
+                let values = this.getValues();
+
+                this.getPhoto().then(
+                    (content)=>{
+                        /*essa função vai receber o conteudo do meu arquivo*/
+                        values.photo = content;
+                        this.addLine(values);
+                    },
+                    (e)=>{
+                        alert(e);
+                    }
+
+                );
+            });
+    }
+
+    getPhoto() {
+        return new Promise( (resolve, reject) => {
+                let fileReader = new FileReader();
+
+                //filtrando elemeentos do formulário apenas por quem for de nome foto
+                let elements = [...this.formEl.elements].filter(
+                    item => {
+                        if (item.name === 'photo') {
+                            return item;
+                        };
+                    }
+                )
+
+                //apontando para o arquivo que está no elemento filtrado
+                let file = elements[0].files[0];
+
+                //após carregar o arquivo será realizada uma função recebida de parametro com o arquivo carregado
+                fileReader.onload = () => {
+                    resolve(fileReader.result);
+                };
+
+                fileReader.onerror = (e) =>{
+                    reject(e);
+                }
+
+                fileReader.readAsDataURL(file);
+            }        
+        )
+
+
     }
 
 
     getValues() {
         let user = {};
         //captura dos dados ao evento submit dentro do formulário
-        this.formEl.elements.forEach(
+        [...this.formEl.elements].forEach(
             function (field, index) {
                 if (field.name == "gender" && field.checked) {
                     user[field.name] = field.value;
@@ -37,10 +82,10 @@ class UserController {
         );
     }
 
-   addLine(dataUser) {
+    addLine(dataUser) {
         this.tableEl.innerHTML = `
             <tr>
-                <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
                 <td>${dataUser.admin}</td>
@@ -50,7 +95,7 @@ class UserController {
                     <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>`
-        ;
+            ;
     }
 
 
